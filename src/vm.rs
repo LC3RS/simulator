de5327338c74157370c7f0b9aceb4b99b338014d
DarkGuy10 <HarperBurn@protonmail.ch>
2025-03-03 21:49:53 +0530

    feat: add fde cycle skeleton [WIP]

---
+++ src/vm.rs
@@ -0,0 +1,67 @@
+use num_traits::FromPrimitive;
+
+use crate::{
+    constants::MAX_MEMORY,
+    enums::{DecodedInstr, RawOpCode},
+};
+
+pub struct Machine {
+    registers: [u16; 8],
+    pc: u16,
+    cond: u16,
+    memory: [u16; MAX_MEMORY],
+    is_running: bool,
+}
+
+impl Default for Machine {
+    fn default() -> Self {
+        Machine {
+            registers: [0; 8],
+            pc: 0,
+            cond: 0,
+            memory: [0; MAX_MEMORY],
+            is_running: false,
+        }
+    }
+}
+
+impl Machine {
+    pub fn print_registers(&self) {
+        for reg in &self.registers {
+            print!("{reg} ");
+        }
+        println!();
+    }
+
+    pub fn run(&mut self) {
+        self.is_running = true;
+
+        while self.is_running {
+            let raw_instr = self.fetch();
+            let decoded_instr = self.decode(raw_instr);
+            self.execute(decoded_instr)
+        }
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let instr = self.memory[self.pc as usize];
+        self.pc += 1;
+        instr
+    }
+
+    fn decode(&self, raw_instr: u16) -> DecodedInstr {
+        let raw_op = RawOpCode::from_u16(raw_instr >> 12).unwrap();
+
+        match raw_op {
+            RawOpCode::Noop => DecodedInstr::Noop,
+            _ => DecodedInstr::Noop, // TODO: remove after complete
+        }
+    }
+
+    fn execute(&self, decoded_instr: DecodedInstr) {
+        // TODO: implement execute
+        match decoded_instr {
+            _ => (),
+        }
+    }
+}
