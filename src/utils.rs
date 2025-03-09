a474aceaf7062773074b7135e0be43b632819ac0
VaibhavR05 <rajvaibhav1205@gmail.com>
2025-03-09 19:27:40 +0530

    feat: add end_swap util function

--- src/utils.rs
+++ src/utils.rs
@@ -10,15 +10,27 @@ pub fn sign_extend(mut x: u16, bit_count: u16) -> u16 {
     x
 }
 
+pub fn end_swap(x: u16) -> u16 {
+    (x >> 8) | (x << 8)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_sign_extend() {
-        assert_eq!(sign_extend(13u16, 5), 13);
-        assert_eq!(sign_extend(13u16, 4), 0b1111_1111_1111_1101);
-        assert_eq!(sign_extend(13u16, 0), 13u16);
         assert_eq!(sign_extend(0u16, 1), 0u16);
+        assert_eq!(sign_extend(13u16, 0), 13u16);
+        assert_eq!(sign_extend(13u16, 5), 13u16);
+        assert_eq!(sign_extend(13u16, 4), 0b1111_1111_1111_1101u16);
+    }
+
+    #[test]
+    fn test_end_swap() {
+        assert_eq!(end_swap(0x6969u16), 0x6969u16);
+        assert_eq!(end_swap(0x6699u16), 0x9966u16);
+        assert_eq!(end_swap(0x6996u16), 0x9669u16);
+        assert_eq!(end_swap(0b1100_1010_1101_0110u16), 0b1101_0110_1100_1010u16);
     }
 }
